8d8ef15e161fe3f5414fecb699b93feae05cd963
Mohamed Azzam <sci.MohamedElsayed32561@alexu.edu.eg>
2026-09-17 02:01:44 +0300

    Update base path in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/zemolibrary/',
+  base: '/zemoLibrary/',
   plugins: [react()],
   server: {
     port: 4200
