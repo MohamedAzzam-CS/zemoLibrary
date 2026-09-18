3bd16ed2cf0eae1a5ac92c0490b8066b49298350
Mohamed Azzam <mohammedazzam172@gmail.com>
2026-09-18 14:29:38 +0300

    Changes to be committed: 	modified:   frontend/src/App.tsx 	deleted:    frontend/src/pages/CommingSoon/ComingSoon.module.scss 	deleted:    frontend/src/pages/CommingSoon/ComingSoon.tsx 	modified:   frontend/tsconfig.tsbuildinfo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,25 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-//import Layout from './components/Layout/Layout';
-//import Home from './pages/Home/Home';
-//import Catalog from './pages/Catalog/Catalog';
-//import BookDetail from './pages/BookDetail/BookDetail';
-//import Search from './pages/Search/Search';
-//import Login from './pages/Login/Login';
-//import Profile from './pages/Profile/Profile';
-//import NotFound from './pages/NotFound/NotFound';
-import ComingSoon from './pages/CommingSoon/ComingSoon';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import Catalog from './pages/Catalog/Catalog';
+import BookDetail from './pages/BookDetail/BookDetail';
+import Search from './pages/Search/Search';
+import Login from './pages/Login/Login';
+import Profile from './pages/Profile/Profile';
+import NotFound from './pages/NotFound/NotFound';
 
 export default function App() {
   return (
-  <Routes>
-    <Route path="/coming-soon" element={<ComingSoon />} />
-
-    {/* Temporarily disabled
-    <Route element={<Layout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/catalog/:id" element={<BookDetail />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>*/}
-
-    <Route
-      path="*"
-      element={<Navigate to="/coming-soon" replace />}
-    />
-  </Routes>
-);
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="*" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/catalog/:id" element={<BookDetail />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
--- frontend/src/pages/CommingSoon/ComingSoon.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './ComingSoon.module.scss';
-
-export default function ComingSoon() {
-  return (
-    <div className={styles.home}>
-      <p></p>
-    </div>
-  );
-}
